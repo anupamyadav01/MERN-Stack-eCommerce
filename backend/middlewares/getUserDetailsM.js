288356bc465e780d24dbe86f8fc07a4fe8976201
Anupam Yadav <anupamy571@gmail.com>
2024-10-09 12:25:05 +0530

    Optional Chaining Handled

--- backend/middlewares/getUserDetailsM.js
+++ backend/middlewares/getUserDetailsM.js
@@ -5,7 +5,7 @@ export const getUserDetails = async (req, res, next) => {
   try {
     const token =
       req?.headers?.cookie?.split("=")[1] ||
-      req.headers.authorization.split(" ")[1];
+      req?.headers?.authorization?.split(" ")[1];
 
     if (!token) {
       return res.status(401).json({
